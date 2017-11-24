e703f4436a82d139f623794b77af98ae19447325
nekocode <nekocode.cn@gmail.com>
2017-11-24 19:06:22 +0800

    Modfify method putValueToBundle

--- meepo/src/main/java/cn/nekocode/meepo/MeepoUtils.java
+++ meepo/src/main/java/cn/nekocode/meepo/MeepoUtils.java
@@ -17,12 +17,19 @@
 package cn.nekocode.meepo;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Size;
+import android.util.SizeF;
+import android.util.SparseArray;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 /**
@@ -47,54 +54,93 @@ public class MeepoUtils {
     }
 
     public static void putValueToBundle(
-            @NonNull Bundle bundle, @NonNull String key, @NonNull Object value) throws RuntimeException {
+            @NonNull Bundle bundle, @NonNull String key, @NonNull Object value) {
 
-        if (value instanceof Integer) {
-            bundle.putInt(key, (Integer) value);
-        } else if (value instanceof Long) {
-            bundle.putLong(key, (Long) value);
-        } else if (value instanceof String) {
-            bundle.putString(key, (String) value);
-        } else if (value instanceof CharSequence) {
-            bundle.putCharSequence(key, (CharSequence) value);
-        } else if (value instanceof Float) {
-            bundle.putFloat(key, (Float) value);
-        } else if (value instanceof Double) {
-            bundle.putDouble(key, (Double) value);
-        } else if (value instanceof Character) {
-            bundle.putChar(key, (char) value);
-        } else if (value instanceof Short) {
-            bundle.putShort(key, (Short) value);
-        } else if (value instanceof Boolean) {
-            bundle.putBoolean(key, (Boolean) value);
-        } else if (value instanceof int[]) {
-            bundle.putIntArray(key, (int[]) value);
-        } else if (value instanceof long[]) {
-            bundle.putLongArray(key, (long[]) value);
-        } else if (value instanceof String[]) {
-            bundle.putStringArray(key, (String[]) value);
-        } else if (value instanceof CharSequence[]) {
-            bundle.putCharSequenceArray(key, (CharSequence[]) value);
-        } else if (value instanceof float[]) {
-            bundle.putFloatArray(key, (float[]) value);
-        } else if (value instanceof double[]) {
-            bundle.putDoubleArray(key, (double[]) value);
-        } else if (value instanceof char[]) {
-            bundle.putCharArray(key, (char[]) value);
-        } else if (value instanceof short[]) {
-            bundle.putShortArray(key, (short[]) value);
-        } else if (value instanceof boolean[]) {
-            bundle.putBooleanArray(key, (boolean[]) value);
+        if (value instanceof IBinder) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                bundle.putBinder(key, (IBinder) value);
+            }
         } else if (value instanceof Bundle) {
             bundle.putBundle(key, (Bundle) value);
+        } else if (value instanceof Boolean) {
+            bundle.putBoolean(key, (boolean) value);
+        } else if (value instanceof boolean[]) {
+            bundle.putBooleanArray(key, (boolean[]) value);
+        } else if (value instanceof Double) {
+            bundle.putDouble(key, (double) value);
+        } else if (value instanceof double[]) {
+            bundle.putDoubleArray(key, (double[]) value);
+        } else if (value instanceof Long) {
+            bundle.putLong(key, (long) value);
+        } else if (value instanceof long[]) {
+            bundle.putLongArray(key, (long[]) value);
+        } else if (value instanceof Byte) {
+            bundle.putByte(key, (byte) value);
+        } else if (value instanceof byte[]) {
+            bundle.putByteArray(key, (byte[]) value);
+        } else if (value instanceof Character) {
+            bundle.putChar(key, (char) value);
+        } else if (value instanceof char[]) {
+            bundle.putCharArray(key, (char[]) value);
+        } else if (value instanceof String) {
+            bundle.putString(key, (String) value);
+        } else if (value instanceof String[]) {
+            bundle.putStringArray(key, (String[]) value);
+        } else if (value instanceof CharSequence) {
+            bundle.putCharSequence(key, (CharSequence) value);
+        } else if (value instanceof CharSequence[]) {
+            bundle.putCharSequenceArray(key, (CharSequence[]) value);
+        } else if (value instanceof Float) {
+            bundle.putFloat(key, (float) value);
+        } else if (value instanceof float[]) {
+            bundle.putFloatArray(key, (float[]) value);
+        } else if (value instanceof Integer) {
+            bundle.putInt(key, (int) value);
+        } else if (value instanceof int[]) {
+            bundle.putIntArray(key, (int[]) value);
         } else if (value instanceof Parcelable) {
             bundle.putParcelable(key, (Parcelable) value);
+        } else if (value instanceof Parcelable[]) {
+            bundle.putParcelableArray(key, (Parcelable[]) value);
+        } else if (value instanceof Short) {
+            bundle.putShort(key, (short) value);
+        } else if (value instanceof short[]) {
+            bundle.putShortArray(key, (short[]) value);
         } else if (value instanceof Serializable) {
             bundle.putSerializable(key, (Serializable) value);
+        } else if (value instanceof Size) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                bundle.putSize(key, (Size) value);
+            }
+        } else if (value instanceof SizeF) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                bundle.putSizeF(key, (SizeF) value);
+            }
+        } else if (value instanceof List) {
+            try {
+                bundle.putIntegerArrayList(key, (ArrayList<Integer>) value);
+            } catch (ClassCastException ignored) {
+            }
+            try {
+                bundle.putStringArrayList(key, (ArrayList<String>) value);
+            } catch (ClassCastException ignored) {
+            }
+            try {
+                bundle.putCharSequenceArrayList(key, (ArrayList<CharSequence>) value);
+            } catch (ClassCastException ignored) {
+            }
+            try {
+                bundle.putParcelableArrayList(key, (ArrayList<? extends Parcelable>) value);
+            } catch (ClassCastException ignored) {
+            }
+        } else if (value instanceof SparseArray) {
+            try {
+                bundle.putSparseParcelableArray(key, (SparseArray<? extends Parcelable>) value);
+            } catch (ClassCastException ignored) {
+            }
         } else {
             throw new RuntimeException(String.format(Locale.getDefault(),
-                    "Arguments extra %s has wrong type %s.",
-                    key, value.getClass().getName()));
+                    "Arguments extra %s has wrong type %s.", key, value.getClass().getName()));
         }
     }
 }
