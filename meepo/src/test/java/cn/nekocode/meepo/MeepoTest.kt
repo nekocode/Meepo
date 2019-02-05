a1fa33c25825046804b422c3170e5bf58eb10c9f
nekocode <nekocode.cn@gmail.com>
2019-02-05 19:00:54 +0800

    Extract test activities

--- meepo/src/test/java/cn/nekocode/meepo/MeepoTest.kt
+++ meepo/src/test/java/cn/nekocode/meepo/MeepoTest.kt
@@ -1,9 +1,27 @@
+/*
+ * Copyright 2019. nekocode (nekocode.cn@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package cn.nekocode.meepo
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import cn.nekocode.meepo.annotation.*
+import cn.nekocode.meepo.annotation.Clazz
+import cn.nekocode.meepo.annotation.ClazzName
+import cn.nekocode.meepo.annotation.Flags
+import cn.nekocode.meepo.annotation.RequestCode
 import cn.nekocode.meepo.config.UriConfig
 import org.junit.Assert
 import org.junit.Test
@@ -14,23 +32,23 @@ import org.robolectric.Shadows
 import org.robolectric.annotation.Config
 import org.robolectric.shadows.ShadowActivity
 
+/**
+ * @author nekocode (nekocode.cn@gmail.com)
+ */
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [21])
 class MeepoTest {
     companion object {
-        const val SCHEMA = "test"
+        const val SCHEME = "test"
         const val HOST = "test.com"
     }
-
-    class AActivity: Activity()
-    class BActivity: Activity()
 
     interface TestRouter {
         @Clazz(BActivity::class)
         @Flags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
         fun clazz(context: Context): Boolean
 
-        @ClazzName("cn.nekocode.meepo.MeepoTest\$BActivity")
+        @ClazzName("cn.nekocode.meepo.BActivity")
         @RequestCode(110)
         fun clazzName(context: Context): Boolean
     }
@@ -40,16 +58,15 @@ class MeepoTest {
         val a = Robolectric.setupActivity(AActivity::class.java)
         val shadowA = Shadows.shadowOf(a)
 
-        val meepo = Meepo.Builder()
-                .config(UriConfig().scheme(SCHEMA).host(HOST))
-                .build()
-
-        val router = meepo.create(TestRouter::class.java)
-
         fun nextActivity() = shadowA.nextStartedActivityForResult
         fun ShadowActivity.IntentForResult.className() = intent.component!!.className
         val bClassName = BActivity::class.java.name
         var next: ShadowActivity.IntentForResult
+
+        val meepo = Meepo.Builder()
+                .config(UriConfig().scheme(SCHEME).host(HOST))
+                .build()
+        val router = meepo.create(TestRouter::class.java)
 
         Assert.assertTrue(router.clazz(a))
         next = nextActivity()
